5f8eb8809d784bfbb4260eed9c2c82b310768af4
Sarah Benabed <sarabenabed9@gmail.com>
2026-07-28 17:29:36 +0300

    product class and controller

---
+++ demo/src/main/java/com/example/demo/Product.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity ;
+import jakarta.persistence.Id ;
+
+@Entity
+public class Product {
+
+    @Id
+    private String id ;
+
+    private String name ;
+    private String size ;
+    private int price ;
+    private String imageURL ;
+
+    public String getName() {
+        return name ;
+    }
+    public void setName(String name) {
+        this.name = name ;
+    }
+
+    public String getId() {
+        return id ;
+    }
+    public void setId(String id) {
+        this.id = id ;
+    }
+
+    public String getSize() {
+        return this.size ;
+    }
+    public void setSize(String size) {
+        this.size = size ;
+    }
+
+    public int getPrice() {
+        return this.price ;
+    }
+    public void setPrice(int price) {
+        this.price = price ;
+    }
+
+    public String getImageURL() {
+        return imageURL ;
+    }
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL ;
+    }
+}
---
+++ demo/src/main/java/com/example/demo/ProductController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.* ;
+import java.util.List ;
+
+@RestController
+@RequestMapping("/products")
+@CrossOrigin
+
+public class ProductController {
+
+    private final ProductRepository productRepository ;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository ;
+    }
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productRepository.findAll() ;
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productRepository.save(product) ;
+    }
+}
