c401a1d30f424c480a07777e3c1bbafa53715503
Sarah Benabed <sarabenabed9@gmail.com>
2026-08-12 23:26:58 +0300

    fix mail sender

--- demo/src/main/java/com/example/demo/VerificationController.java
+++ demo/src/main/java/com/example/demo/VerificationController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.aspectj.weaver.patterns.IVerificationRequired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.* ;
 import java.util.List ;
 
@@ -16,8 +18,20 @@ public class VerificationController {
         this.service = service ;
     }
     @PostMapping("/send")
-    public String sendCode(@RequestParam String email) {
-        service.sendCode(email);
-        return "code sent !" ;
+    public ResponseEntity<String> sendCode(@RequestParam String email) {
+        System.out.println("inside controller");
+        try {
+            service.sendCode(email);
+            System.out.println("email sent !");
+            return ResponseEntity.ok("Code sent!");
+        }
+        catch (Exception e) {
+            e.printStackTrace(); // prints full stack trace
+
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getClass().getName() + "\n" + e.getMessage());
+        }
     }
+
 }
