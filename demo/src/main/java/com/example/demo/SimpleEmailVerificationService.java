b9809767644c9fe3d8a07d65ae4e21757bd7004c
Sarah Benabed <sarabenabed9@gmail.com>
2026-08-13 01:11:46 +0300

    adding Brevo

--- demo/src/main/java/com/example/demo/SimpleEmailVerificationService.java
+++ demo/src/main/java/com/example/demo/SimpleEmailVerificationService.java
@@ -1,30 +1,47 @@
 package com.example.demo;
 
-import org.springframework.mail.SimpleMailMessage ;
-import org.springframework.mail.javamail.JavaMailSender ;
-import org.springframework.stereotype.Service ;
-import java.util.* ;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.*;
 
 @Service
 public class SimpleEmailVerificationService {
 
-    private final JavaMailSender mailSender ;
+    @Value("${BREVO_API_KEY}")
+    private String apiKey ;
+
     private final Map<String, String> codes = new HashMap<>() ;
 
-    public SimpleEmailVerificationService(JavaMailSender mailSender) {
-        this.mailSender = mailSender ;
-    }
+    private final RestTemplate restTemplate = new RestTemplate() ;
+
 
     public void sendCode(String email) {
         String code = String.valueOf(new Random().nextInt(900000) + 100000) ;
         codes.put(email, code) ;
 
-        SimpleMailMessage message = new SimpleMailMessage() ;
-        message.setTo(email);
-        message.setSubject("Verification code");
-        message.setText("Your Varification code is : " + code);
+        HttpHeaders headers = new HttpHeaders() ;
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("api-key", apiKey);
 
-        mailSender.send(message);
+        Map<String, Object> body = new HashMap<>() ;
+        body.put("sender", Map.of("name","Thrift Store","email", "sarabenabed9@gmail.com")) ;
+        body.put("to", List.of(Map.of("email", email))) ;
+
+        body.put("subject", "Verification Code") ;
+        body.put("textContent", "Your Verification Code is : " + code) ;
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers) ;
+        try {
+            restTemplate.postForEntity("https://api.brevo.com/v3/smtp/email", request, String.class);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean verifyCode(String email, String inputCode) {
